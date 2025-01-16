1f9c2d2ecbdc7b2655c4c76b4f471530d521c23f
mariam <mariam@email.com>
2025-01-16 14:11:27 +0100

    sexto ejercicio

--- ej6.py
+++ ej6.py
@@ -1,10 +1,11 @@
+import random as r
 notas = []
 suma = 0
 aprobados=0
 suspensos=0
 supera=0
 for i in range(40):
-    notas.append(int(input(f"Introduce la nota del alumno {i}: ")))
+    notas.append(r.randint(0,11))
     suma += notas[i]
 for i in range(40):
     if notas[i]>=5:
@@ -13,4 +14,4 @@ for i in range(40):
             supera += 1
     else:
         suspensos+=1
-print(f"{aprobados} alumnos han aprobado, {suspensos} han suspendido y {supera} superan la nota media de la clase, que es {suma/40}.")
+print(f"Las notas son: {notas}\n{aprobados} alumnos han aprobado, {suspensos} han suspendido y {supera} superan la nota media de la clase, que es {suma/40}.")
